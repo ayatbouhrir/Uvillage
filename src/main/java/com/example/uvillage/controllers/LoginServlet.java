dadf3a729d3c1eaff5ff06442f40e59cff8d4197
Jalal_Zerroudi <jalal.zerroudi@usmba.ac.ma>
2024-08-01 17:27:17 +0200

    commit :: Modify

--- src/main/java/com/example/uvillage/controllers/LoginServlet.java
+++ src/main/java/com/example/uvillage/controllers/LoginServlet.java
@@ -11,6 +11,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
+import javax.servlet.ServletContext;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -28,7 +29,7 @@ public class LoginServlet extends HttpServlet {
     public void init() throws ServletException {
         super.init();
         configureLogger();
-        WebApplicationContext context = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
+        WebApplicationContext context = WebApplicationContextUtils.getRequiredWebApplicationContext((ServletContext) getServletContext());
         databaseUtil = context.getBean(DatabaseUtil.class);
     }
 
